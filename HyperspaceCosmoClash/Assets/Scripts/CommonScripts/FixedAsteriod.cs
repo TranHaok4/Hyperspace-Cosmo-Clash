49c0e0d7a87b3ae012dee331528c5cffa50e6c37
TranHaok4 <tranphuchao2004@gmail.com>
2024-02-20 10:02:56 +0700

    Day36:Make FixedAsteriod block bullet

---
+++ HyperspaceCosmoClash/Assets/Scripts/CommonScripts/FixedAsteriod.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedAsteriod : HaroMonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer==LayerMask.NameToLayer("EnemyBullet") || collision.gameObject.layer == LayerMask.NameToLayer("PlayerBullet"))
+        {
+            Debug.Log(collision.name);
+            BulletCtrl bulletOBJ = collision.gameObject.transform.parent.GetComponent<BulletCtrl>();
+            if(bulletOBJ != null)
+            {
+                bulletOBJ.BulletDespawn.DespawnObject();
+            }
+            else
+            {
+                Debug.Log("haha");
+            }
+        }
+    }
+}
